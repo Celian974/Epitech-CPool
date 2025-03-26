5206ddca8c3bae589d1dbd23b769f3d08043f5dd
Celian Siampirave <celian.siampirave@epitech.eu>
2025-03-27 00:11:20 +0100

    [ADD] my_compute_factorial_it prototype

--- include/cpool.h
+++ include/cpool.h
@@ -7,11 +7,16 @@
 
 #ifndef CPOOL_H_
     #define CPOOL_H_
+
+//exercises from Day 4 of the CPool
 void my_putchar(char c);
 int my_putstr(char const *str);
 void my_swap(int *a, int *b);
 int my_strlen(char const *str);
 char *my_evil_str(char *str);
 void my_sort_int_array(int *array, int size);
+
+//exercises from Day 5 of the CPool
+int my_compute_factorial_it(int nb);
 
 #endif
