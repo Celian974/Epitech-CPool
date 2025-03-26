cc7a0b694ef905fb69d97421f7def38b2a9419ff
Celian Siampirave <celian.siampirave@epitech.eu>
2025-03-26 23:57:47 +0100

    [ADD] my_sort_int_array prototype

--- include/cpool.h
+++ include/cpool.h
@@ -12,5 +12,6 @@ int my_putstr(char const *str);
 void my_swap(int *a, int *b);
 int my_strlen(char const *str);
 char *my_evil_str(char *str);
+void my_sort_int_array(int *array, int size);
 
 #endif
